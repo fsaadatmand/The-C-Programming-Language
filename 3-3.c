6fd996682dee0bbb41eebe5db5dfeebd4ae2aa37
cdude <cdude996@gmail.com>
2018-08-09 11:51:31 -0400

    Rewrite WIP

--- 3-3.c
+++ 3-3.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 
 #define MAXLINE 1000
+#define SPACE ' '
+#define TAB '\t'
+#define COMMA ','
+#define DOT '.'
 
 /* readline function: read a line into s, return length */
 int readline(char s[], int lim)
@@ -21,38 +26,51 @@ int readline(char s[], int lim)
 	return i;
 }
 
-int expand(char s1[], char s2[])
+void expand(char s1[], char s2[])
 {
-	int i, j, trailing, position, foundMatch;
+	int i;                /* current input line index */
+	int j;                /* current output line index */
+	int p;                /* temp index for minus positon */
+	int position = 0;         /* position of the minus char */
+	int notShortHand = 0;
+	int isShortHand = 0;
 	char minus = '-';
 
-//	do {
-//			position = ++i;               
-//	} while (s1[i] != ' ');         /* first occurrence of - in s1 */
 
 	for (i = 0; s1[i] != '\0'; ++i) {
-		if (line[i] == minus && line[i - 1] != ' ') {
-			foundMatch = 0;
-			for (j = i + 1; s1[j] != '\0' && foundMatch != 1; ++j)
-				if (s1[j] == minus) 
-					if (s1[j - 1] == ' ')
-						trailing = 1;
-					else {
-						position = j;
-						foundMatch = 1;
-					}
+		if (s1[i] == minus && s1[i - 1] != SPACE && s1[i + 1] != SPACE)  /* skip leading & trailing minus */
+			if (s1[i - 2] == SPACE || s1[i - 2] == TAB && s1[i + 2] == SPACE || s1[i + 2] == '\t') {
+				isShortHand = 1;
+				position = i;
+			}
+//			else
+//				for (p = i + 1; s1[p] != ' ' && s1[p] != '\t'; ++p)     /* check for more minuses - */
+//					if (s1[p] == minus)
+//						isShortHand = 0;
+//					else {
+//						isShortHand = 1;
+//						position = i;
+//					}
 
-		}
+		if (isShortHand) {
+			for (p = s1[position - 1]; p < s1[position + 1]; ++p) {
+				s2[j] = p;
+				++j;
+			}
+			++i;
+		} else
+			s2[j] = s1[i];
+		++j;
 	}
-	return position;
 }
 int main(void)
 {
 	int position;
-	char line[MAXLINE] = { " -a thi-s a test for a-b.\n" };
+	char line[MAXLINE] = { " -a th-i-s a-b is a test for a-b.\n" };
 	char modLine[MAXLINE];
 
-	printf("%i\n", expand(line, modLine));
+	expand(line, modLine);
+	printf("%s\n", modLine);
 	
 	return 0;
 }
