c4f49d0c2a57799ab91b6063e2a16218d8b991cc
cdude <cdude996@gmail.com>
2018-08-14 12:56:56 -0400

    Initial commit - WIP

---
+++ 4-3.c
@@ -0,0 +1,151 @@
+/* 
+ * Exercise 4-3. Given the basick framwork, it's straightforward to extend the
+ * calculator. Add the modulos (%) operator and provisions for negative numbers
+ */
+
+#include <stdio.h>
+#include <stdlib.h>          /* for atof() */
+#include <ctype.h>
+
+#define MAXOP   100          /* max size of operand or operator */
+#define NUMBER  '0'          /* signal that a number was found */
+#define MAXVAL  100          /* maximum depth of val stack */
+#define BUFSIZE 100
+
+/* function decleration */
+int    getop(char []);
+void   push(double);
+double pop(void);
+int    getch(void);
+void   ungetch(int);
+
+/* external variables */
+int    sp = 0;               /* next free stack position */
+double val[MAXVAL];          /* value stack */
+char   buf[BUFSIZE];         /* buffer from ungetch */
+int    bufp = 0;             /* next free position in buf */
+int    sign = 1;
+
+/* push: push f onto value stack */
+void push(double f)
+{
+	if (sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return top value from stack */
+double pop(void)
+{
+	if (sp > 0)
+		return val[--sp];
+	else {
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
+
+/* getop: get next operator or numeric operand */
+int getop(char s[])
+{
+	int i, c, temp;
+
+	while ((s[0] = c = getch()) == ' ' || c == '\t' || c == '-')
+		if (c == '-')
+			sign = -1;
+	s[1] = '\0';
+
+	if (sign == -1)
+		s[0] = '-';
+	/*
+	if (c == '\n') {
+		printf("buffer %i\n", buf[bufp]);
+		printf("bufp %i\n", bufp);
+	}
+	*/
+
+	if (c == '\n' && sign == -1) {
+		ungetch(c);
+		sign = 1;
+		return '-';
+	} else (sign == -1) {
+		s[0] = '-';
+		s[1] = c;
+	} else if (!isdigit(c) && c != '.' && sign != -1)
+		return c;            /* not a number */
+
+
+	i = 0;
+	if (isdigit(c))
+		while (isdigit(s[++i] = c = getch()))
+			;
+	if( c == '.')            /* collect fraction part */
+		while (isdigit(s[++i] = c = getch()))
+			;
+	s[i] = '\0';
+	if (c != EOF)
+		ungetch(c);
+	return NUMBER;
+}
+
+int getch(void)              /* get a (possibly pushed back) character */
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)          /* push character back on input */
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
+/* reverse Polish Calculator */
+int main(void)
+{
+	int type;
+	double op2;
+	char s[MAXOP];
+
+	while ((type = getop(s)) != EOF) {
+		switch (type) {
+		case NUMBER:
+			push(sign * atof(s));
+			sign = 1;
+			break;
+		case '+':
+			push(pop() + pop());
+			break;
+		case '*':
+			push(pop() * pop());
+			break;
+		case '-':
+			op2 = pop();
+			push(pop() - op2);
+			break;
+		case '/':
+			op2 = pop();
+			if (op2 != 0.0)
+				push(pop() / op2);
+			else
+				printf("error: zero divisor\n");
+			break;
+		case '%':
+			op2 = pop();
+			if (op2 != 0.0)
+				push((long) pop() % (long) op2);
+			else
+				printf("error: zero divisor\n");
+			break;
+		case '\n':
+			printf("\t%.8g\n", pop());
+			break;
+		defualt:
+			printf("error: unknown comman %s\n", s);
+			break;
+		}
+	}
+	return 0;
+}
