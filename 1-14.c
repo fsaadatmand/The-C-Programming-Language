72a0b86676c9d34fe5ace23d051eaad030ed74e5
cdude <cdude996@gmail.com>
2018-08-18 21:56:16 -0400

    Define constant for loop index

--- 1-14.c
+++ 1-14.c
@@ -6,29 +6,27 @@
 
 #include <stdio.h>
 
+#define MAXCHAR 127
 #define EMPTY 0
 
 int main(void)
 {
 	int c, i, j, longestBar;
-	int characters[127];
+	int characters[MAXCHAR];
 
-	for (i = 0; i <= 127; ++i)          /* initialize elements' values to 0 */
+	for (i = 0; i <= MAXCHAR; ++i)          /* initialize elements' values to 0 */
 		characters[i] = 0;
 
 	while ((c = getchar()) != EOF)
-		if (c > 127)                    /* non standard ASCII character */
-			++characters[127];
-		else
-			++characters[c - '0'];
+		++characters[c - '0'];
 
-	for (i = 0; i <= 127; ++i)          /* print value of each element (bar) */
+	for (i = 0; i <= MAXCHAR; ++i)          /* print value of each element (bar) */
 		printf("%i ", characters[i]);
 	printf("\n");
 
 	printf("\nHorizontal Histogram:\n");
 
-	for (i = 0; i <= 127; ++i) {
+	for (i = 0; i <= MAXCHAR; ++i) {
 		if (characters[i] != EMPTY)     /* skip label if no data */
 			printf("%c ", i + '0');     /* print histogram labels */
 		for (j = 1; j <= characters[i]; ++j)
@@ -40,22 +38,23 @@ int main(void)
 	printf("\nVertical Histogram:\n");
 
 	longestBar = characters[0];
-	for (i = 0; i <= 127; ++i)          /* find the longestBar */
+	for (i = 0; i <= MAXCHAR; ++i)          /* find the longestBar */
 		if (longestBar < characters[i] )
 			longestBar = characters[i];
 
 	while (longestBar != 0) {           /* print vertical histogram  */
-		for (i = 0; i <= 127; ++i)
-			if (characters[i] != EMPTY)
+		for (i = 0; i <= MAXCHAR; ++i)
+			if (characters[i] != EMPTY) {
 				if (characters[i] - longestBar < 0)
 					printf("  ");
 				else
 					printf("* ");
+			}
 		--longestBar;
 		printf("\n");
 	}
 
-	for (i = 0; i <= 127; ++i)          /* print histogram labels */
+	for (i = 0; i <= MAXCHAR; ++i)          /* print histogram labels */
 		if (characters[i] != EMPTY)
 			printf ("%c ", i + '0');
 		printf("\n");
