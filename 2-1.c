a210954c29d777507347117e69e6862cee3ca33a
cdude <cdude996@gmail.com>
2018-09-18 23:33:39 -0400

    Code clean up. Float, double, long double are missing.
    For some reason I seem to have lost my computations for floating point
    ranges.  Oh well, will rewrite.

--- 2-1.c
+++ 2-1.c
@@ -1,103 +1,99 @@
-/* A program that computes and prints value rages for all the variable types */
+/*
+ * Exercise 2-1. Write a program to determine the ranges of char, short, int,
+ * and long variables, both signed and unsigned, by printing appropriate values
+ * from standard headers and by direct computation. Harder if you compute them:
+ * determine the ranges of the various floating-point types.
+ * By Faisal Saadatmand
+*/
 
 #include <stdio.h>
 #include <limits.h>
 #include <float.h>
+#include <math.h>
 
 void computeRanges(void)
 {
-	signed char          sChar;
-	unsigned char        uChar;
-	signed short         shrtInt;
-	unsigned short       uShrtInt;
-	signed int           sInt;
-	unsigned int         uInt;
-	signed long          lInt;
-	unsigned long        uLInt;
-	signed long long     lLInt;
-	unsigned long long   uLLInt;
-	long                 maxValue;
-	long                 minValue;
+	/* characters */
+	signed char c; 
+	int maxC = 1;
+	for (c = 1; c > 0; c *= 2)
+		maxC *= 2;
+	printf("signed char\t\t%i\t\t\t%i\n", c, maxC - 1);
 
-	maxValue = 1;
-	for (sChar = 1; sChar > 0; sChar *= 2)
-		maxValue *= 2;
-	for (sChar = -1; sChar < 0; sChar *= 2)
-		minValue = sChar;
-	printf("signed char\t%i\t\t\t%i\n", minValue, maxValue - 1);
+	unsigned char uc;
+	for (uc = -1; uc < 0; uc *= 2)
+		;
+	printf("unsigned char\t\t%2i\t\t\t%i\n", 0, uc);
 
-	maxValue = 1;
-	for (uChar = 1; uChar > 0; uChar *= 2)
-		maxValue *= 2;
-	printf("unsigned char\t%2i\t\t\t%u\n", 0, maxValue - 1);
+	/* Integers */
+	signed short  shrt;
+	int maxShrt = 1;
+	for (shrt = 1; shrt > 0; shrt *= 2)
+		maxShrt *= 2;
+	printf("signed short\t\t%i\t\t\t%i\n", shrt, maxShrt - 1);
 
-	maxValue = 1;
-	for (shrtInt = 1; shrtInt > 0; shrtInt *= 2)
-		maxValue *= 2;
-	for (shrtInt = -1; shrtInt < 0; shrtInt *= 2)
-		minValue = shrtInt;
-	printf("signed short\t%i\t\t\t%i\n", minValue, maxValue - 1);
+	unsigned short uShrt;
+	for (uShrt = -1; uShrt < 0; uShrt *= 2)
+		;
+	printf("unsigned short\t\t%2i\t\t\t%i\n", 0, uShrt);
 
-	maxValue = 1;
-	for (uShrtInt = 1; uShrtInt > 0; uShrtInt *= 2)
-		maxValue *= 2;
-		minValue = shrtInt;
-	printf("unsigned short\t%2i\t\t\t%u\n", 0, maxValue - 1);
-
-	maxValue = 1;
+	signed int sInt;
+	int max_sInt = 1;
 	for (sInt = 1; sInt > 0; sInt *= 2)
-		maxValue *= 2;
-	for (sInt = -1; sInt < 0; sInt *= 2)
-		minValue = sInt;
-	printf("signed int\t%i\t\t%i\n", minValue, maxValue - 1);
+		max_sInt *= 2;
+	printf("signed int\t\t%i\t\t%i\n", sInt, max_sInt - 1);
 
-	maxValue = 1;
-	for (uInt = 1; uInt > 0; uInt *= 2)
-		maxValue *= 2;
-	printf("unsigned int\t%2i\t\t\t%u\n", 0, maxValue - 1);
+	unsigned int uInt;
+	for (uInt = -1; uInt < 0; uInt *= 2)
+		;
+	printf("unsigned int\t\t%2i\t\t\t%u\n", 0, uInt);
 
-	maxValue = 1;
+	signed long int lInt;
+	signed long int max_lInt = 1;
 	for (lInt = 1; lInt > 0; lInt *= 2)
-		maxValue *= 2;
-	for (lInt = -1; lInt < 0; lInt *= 2)
-		minValue = lInt;
-	printf("signed long\t%li\t%li\n", minValue, maxValue - 1);
+		max_lInt *= 2;
+	printf("signed long\t\t%li\t%li\n", lInt, max_lInt - 1);
 
-	maxValue = 1;
-	for (uLInt = 1; uLInt > 0; uLInt *= 2)
-		maxValue *= 2;
-	printf("unsigned long\t%2i\t\t\t%lu\n", 0, maxValue - 1);
+	unsigned long int ulInt;
+	for (ulInt = -1; ulInt < 0; ulInt *= 2)
+		;
+	printf("unsigned long\t\t%2i\t\t\t%lu\n", 0, ulInt);
 
-	maxValue = 1;
-	for (lLInt = 1; lLInt > 0; lLInt *= 2)
-		maxValue *= 2;
-	for (lLInt = -1; lLInt < 0; lLInt *= 2)
-		minValue = lLInt;
-	printf("signed long long\t%lli\t%lli\n", minValue, maxValue - 1);
+	signed long long llInt;
+	signed long long max_llInt = 1;
+	for (llInt = 1; llInt > 0; llInt *= 2)
+		max_llInt *= 2;
+	printf("signed long long\t%lli\t%lli\n", llInt, max_llInt - 1);
 
-	maxValue = 1;
-	for (uLLInt = 1; uLLInt > 0; uLLInt *= 2)
-		maxValue *= 2;
-	printf("unsigned long long\t%2i\t\t\t%llu\n", 0, maxValue - 1);
+	unsigned long long   ullInt;
+	for (ullInt = -1; ullInt < 0; ullInt *= 2)
+		;
+	printf("unsigned long long\t%2i\t\t\t%llu\n", 0, ullInt);
+
+	/* floating points */
 }
 
 void stdLibraryRanges(void)
 {
-	printf("signed char\t\t%i\t%i\n", CHAR_MIN, CHAR_MAX);
-	printf("unsigned char\t\t%2i\t%u\n", 0, UCHAR_MAX);
-	printf("signed short\t\t%i\t%i\n", SHRT_MIN, SHRT_MAX);
-	printf("unsigned short\t\t%2i\t%u\n", 0, USHRT_MAX);
-	printf("signed int\t\t%i\t%i\n", INT_MIN, INT_MAX);
-	printf("unsigned int\t\t%2i\t%u\n", 0, UINT_MAX);
+	/* characters */
+	printf("signed char\t\t%i\t\t\t%i\n", CHAR_MIN, CHAR_MAX);
+	printf("unsigned char\t\t%2i\t\t\t%u\n", 0, UCHAR_MAX);
+	/* integers */
+	printf("signed short\t\t%i\t\t\t%i\n", SHRT_MIN, SHRT_MAX);
+	printf("unsigned short\t\t%2i\t\t\t%u\n", 0, USHRT_MAX);
+	printf("signed int\t\t%i\t\t%i\n", INT_MIN, INT_MAX);
+	printf("unsigned int\t\t%2i\t\t\t%u\n", 0, UINT_MAX);
 	printf("signed long\t\t%li\t%li\n", LONG_MIN, LONG_MAX);
-	printf("unsigned long\t\t%2i\t%lu\n", 0, ULONG_MAX);
+	printf("unsigned long\t\t%2i\t\t\t%lu\n", 0, ULONG_MAX);
 	printf("signed long long\t%lli\t%lli\n", LLONG_MIN, LLONG_MAX);
-	printf("unsigned long long\t%2i\t%llu\n", 0, ULLONG_MAX);
+	printf("unsigned long long\t%2i\t\t\t%llu\n", 0, ULLONG_MAX);
 
 	printf("\n");
-	printf("float\t\t%f\t%g\n", FLT_MIN, FLT_MAX);
-	printf("double\t\t%lf\t%lg\n", DBL_MIN, DBL_MAX);
-	printf("long double\t%llf\t%llg\n", LDBL_MIN, LDBL_MAX);
+
+	/* floating points */
+	printf("float\t\t\t%f\t\t%g\n", FLT_MIN, FLT_MAX);
+	printf("double\t\t\t%lf\t\t%lg\n", DBL_MIN, DBL_MAX);
+	printf("long double\t\t%llf\t\t%llg\n", LDBL_MIN, LDBL_MAX);
 }
 
 int main(void)
@@ -108,6 +104,5 @@ int main(void)
 	printf("\n\t**** Library function Ranges *****\n");
 	stdLibraryRanges();
 
-	printf("Size of in %d bytes\n", sizeof(int));
 	return 0;
 }
