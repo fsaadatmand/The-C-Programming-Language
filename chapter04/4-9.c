c45f1ed054fc4c49872603cf7cd3b1ac71d95413
cdude <cdude996@gmail.com>
2018-09-25 15:02:58 -0400

    Correct question description for the exercise

--- chapter04/4-9.c
+++ chapter04/4-9.c
@@ -1,6 +1,7 @@
 /*
- * Exercise 4-9. Suppose that there will never be more than one character of
- * pushback. Modify getch and ungetch accordingly.
+ * Exercise 4-9. Our getch and ungetch do not handle a pushed-back EOF
+ * correctly. Decide what their properties ought to be if an EOF is pushed
+ * back, then implement your design. 
  * Faisal Saadatmand
  */
 
