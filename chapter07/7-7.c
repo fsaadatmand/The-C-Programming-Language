4c7f64eeb927b2f19b9b1f1a13fa49f884fb32c1
cdude <cdude996@gmail.com>
2019-02-15 17:35:24 -0500

    Initial commit
    completed exercise. Will work on cleaning up the code a little.

---
+++ chapter07/7-7.c
@@ -0,0 +1,104 @@
+/*
+ * Exercise 7-7. Modify the pattern finding program of Chapter 5 to take its
+ * input from a set named files or, if no files are named as arguments, from
+ * the standard input. Should the file name be printed when a matching line is
+ * found?
+ * Answer: of course!
+ *
+ * By Faisal Saadatmand
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXLINE 1000
+
+/* fucntions */
+int getLine(char *, int);
+
+/* getLine: get line into s, return length */
+int getLine(char *line, int max)
+{
+	int c, i;
+
+	i = 0;
+	while (--max > 0 && (c = getchar()) != EOF && c != '\n')
+		line[i++] = c;
+	if (c == '\n')
+		line[i++] = c;
+	line[i] = '\0';
+	return i;
+}
+
+/* fgetLine: read line, return length - fgets version */
+int fgetLine(FILE *fp, char *line, int max)
+{
+	if (fgets(line, max, fp) == NULL)
+		return 0;
+	else
+		return strlen(line);
+}
+
+/* find: print lines that match pattern from 1s arg */
+int main(int argc, char *argv[])
+{
+	char line[MAXLINE];
+	long lineno = 0;
+	int c, except = 0, number = 0, found = 0;
+	char *prog = argv[0];
+	char *pattern;
+	FILE *fp;
+
+	while (--argc > 0 && (*++argv)[0] == '-')
+		while ((c = *++argv[0]))
+			switch (c) {
+			case 'x':
+				except = 1;
+				break;
+			case 'n':
+				number = 1;
+				break;
+			default:
+				printf("find: illegal option %c\n", c);
+				argc = 0;
+				found = -1;
+				break;
+			}
+
+	if (argc < 1)
+		printf("Usage: find -x -n pattern\n");
+	else if (argc == 1)
+		while (getLine(line, MAXLINE) > 0) {
+			lineno++;
+			if ((strstr(line, *argv) != NULL) != except) {
+				if (number)
+					printf ("%ld:", lineno);
+				printf("%s", line);
+				found++;
+			}
+		} 
+	else {
+		pattern = *argv;               /* save a point to the pattern */
+		while (argc-- > 1) {
+			if ((fp = fopen(*++argv, "r")) == NULL) {
+				fprintf(stderr, "%s: can't open %s\n", prog, *argv);
+				exit(EXIT_FAILURE);
+			} 
+			lineno = 0;
+			while (!feof(fp))
+				while (fgetLine(fp, line, MAXLINE) > 0) {
+					lineno++;
+					if ((strstr(line, pattern) != NULL) != except) {
+						printf("%s:", *argv);    /* print file name */
+						if (number)
+							printf ("%ld:", lineno);
+						printf("%s", line);
+						found++;
+					}
+				}
+			fclose(fp);
+		}
+	}
+	return found;
+}
