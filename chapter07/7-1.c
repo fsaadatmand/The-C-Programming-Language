7cd6ae5b46ccfbff24ecf9c08dcb928f57188bc6
cdude <cdude996@gmail.com>
2019-02-17 16:48:09 -0500

    minor tweaks

--- chapter07/7-1.c
+++ chapter07/7-1.c
@@ -12,22 +12,22 @@
 int main(int argc, char *argv[])
 {
 	int c, function;
-	char *p;
+	char *pname;                       /* program name */
 
 	if (argc != 1)
 		return -1;
 
-	p = (strrchr(*argv, '/')) + 1;     /* parse program name */
+	pname = (strrchr(*argv, '/')) + 1; /* parse program name */
 
-	if (strcmp("tolower", p) == 0)
+	if (strcmp("tolower", pname) == 0)
 		function = 0;
-	else if (strcmp("toupper", p) == 0)
+	else if (strcmp("toupper", pname) == 0)
 		function = 1;
 	else {
-		printf("Unknown function:\nRename program to tolower or toupper.\n");
+		printf("%s: Unknown function: ", pname);
+		printf("Rename program to tolower or toupper.\n");
 		return -1;
 	}
-
 	while ((c = getchar()) != EOF)
 		putchar((function) ? toupper(c) : tolower(c)); 
 	return 0;
