400e243d9ea053ae8cfc6c1023153e89f8cea081
cdude <cdude996@gmail.com>
2019-03-05 17:36:58 -0500

    Rewrite solution (WIP)
    Still work in progress.

--- chapter05/5-20.c
+++ chapter05/5-20.c
@@ -11,12 +11,14 @@
 #define MAXTOKEN    100
 #define BUFSIZE     100
 
-enum { NAME, PARENS, BRACKETS, TYPE };
+#define SKIP_BLANKS(c)  while (((c) = getch()) == ' ' || (c) == '\t')
+
+enum { NAME, PARENS, BRACKETS };
 
 /* functions */
 int  gettoken(void);
-int  dcl(void);
-int  dirdcl(void);
+void dcl(void);
+void dirdcl(void);
 int  getch(void);
 void ungetch(int);
 
@@ -28,6 +30,7 @@ char datatype[MAXTOKEN];               /* data type = char, int, etc. */
 char out[1000];
 char buf[BUFSIZE];                     /* buffer from ungetch */
 int  bufp = 0;                         /* next free position in buf */
+int  error;                            /* signals parsing failure */
 
 /* gettoekn: return next token */
 int gettoken(void)
@@ -36,10 +39,11 @@ int gettoken(void)
 	void ungetch(int);
 	char *p = token;
 
-	while ((c = getch()) == ' ' || c == '\t')
-		;
+	SKIP_BLANKS(c);
+
 	if (c == '(') {
-		if ((c = getch()) == ')') {
+		SKIP_BLANKS(c);                /* allow spaces in parens */
+		if (c == ')') {
 			strcpy(token, "()");
 			return tokentype = PARENS;
 		} else {
@@ -47,17 +51,19 @@ int gettoken(void)
 			return tokentype = '(';
 		}
 	} else if (c == '[') {
-		for (*p++ = c; (*p++ = getch()) != ']'; )
-			;
-		*p = '\0';
+		for (*p++ = c; (*p = getch()) != ']'; p++)
+			if (*p == '\n')            /* error check: missing ']' */
+				return *p;
+		*++p = '\0';
 		return tokentype = BRACKETS;
 	} else if (isalpha(c)) {
-		for (*p++ = c; isalnum(c = getch()); )
-			*p++ = c;
+		for (*p++ = c; isalnum(c = getch()); p++)
+			*p = c;
 		*p = '\0';
 		ungetch(c);
 		return tokentype = NAME;
-	} else return tokentype = c;
+	} else
+		return tokentype = c;
 }
 
 /* getch: get a (possibly pushed back) character */
@@ -70,7 +76,7 @@ int getch(void)
 void ungetch(int c)
 {
 	if (c == EOF)
-		bufp = 0;            /* clear buffer */
+		bufp = 0;                      /* clear buffer */
 
 	if (bufp >= BUFSIZE && c != EOF)
 		printf("ungetch: too many characters\n");
@@ -79,61 +85,65 @@ void ungetch(int c)
 }
 
 /* dcl: parse a declarator */
-int dcl (void)
+void dcl (void)
 {
 	int ns;
 
 	for (ns = 0; gettoken() == '*'; )  /*count *'s */
 		ns++;
-	if (dirdcl() > 0) {
-		while (ns-- > 0)
-			strcat(out, " pointer to");
-	return 0;
-	} else
-		return -1;
+	dirdcl();
+	while (ns-- > 0)
+		strcat(out, " pointer to");
 }
 
 /* dirdcl: parse a direct declarator */
-int dirdcl(void)
+void dirdcl(void)
 {
 	int type;
 	
+	error = 0;
 	if (tokentype == '(') {            /* ( dcl ) */
-		if (dcl () < 0 || tokentype != ')') {
-//		if (tokentype != ')') {
+		dcl ();
+		if (tokentype != ')') {
 			printf("error: missing )\n");
-			return -1;
+			error = 1;
 		}
-	} else if (tokentype == NAME) {    /* variable name */
+	} else if (tokentype == NAME)      /* variable name */
 		strcpy(name, token);
-	} else {
+	else {
 		printf("error: expected name or (dcl)\n");
-		return -1;
+		error = 1;
 	}
-	while ((type = gettoken()) == PARENS || type == BRACKETS)
-		if (type == PARENS)
-			strcat(out, " function returning");
-		else {
+
+	if (!error) {
+		while ((type = gettoken()) == PARENS || type == BRACKETS)
+			if (type == PARENS)
+				strcat(out, " function returning");
+			else {
 			strcat(out, " array");
 			strcat(out,  token);
 			strcat(out, " of");
-		}
-	return 1;
+			}
+	}
 }
+
+// parameter-type-list      loop that gets the tokens include ',' and stops at ')'
+// parameter-declaration    checks tokens according to grammer
+// declaration-specifiers   datatype?
 
 /* convert declaration to words */
 int main(void)               
 {
-	while (gettoken() != EOF) {                  /* last token on line */
-		strcpy(datatype, token);                 /* is the datatype */
+	while (gettoken() != EOF) {        /* last token on line */
+		strcpy(datatype, token);       /* is the datatype */
 		out[0] = '\0';
-		if (dcl() < 0 || tokentype != '\n') {    /* parse rest of line */
-			printf("syntax error\n");
-			/* error recovery: reset pointers */
-			name[0] = out[0] = datatype[0] = token[0] = buf[0] = '\0';
-			bufp = tokentype = 0;
-		} else
-			printf("%s: %s %s\n", name, out, datatype);
+		if (tokentype != '\n') {       /* skip empty input lines */
+			dcl();
+			if (tokentype != '\n')
+				printf("syntax error\n");
+			else if (!error)
+				printf("%s: %s %s\n", name, out, datatype);
+		}
 	}
 	return 0;
 }
