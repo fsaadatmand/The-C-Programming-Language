8dbd9878c18070d0ee6dbcc8b4130d585b0638ca
cdude <cdude996@gmail.com>
2019-03-10 16:21:05 -0400

    fix stack error when filling characters[]. other minor tweaks.

--- chapter01/1-14.c
+++ chapter01/1-14.c
@@ -6,7 +6,7 @@
 
 #include <stdio.h>
 
-#define MAXCHAR 127
+#define MAXCHAR 128
 #define EMPTY 0
 
 int main(void)
@@ -14,36 +14,37 @@ int main(void)
 	int c, i, j, longestBar;
 	int characters[MAXCHAR];
 
-	for (i = 0; i <= MAXCHAR; ++i)          /* initialize elements' values to 0 */
+	for (i = 0; i <= MAXCHAR; i++)     /* initialize elements' values to 0 */
 		characters[i] = 0;
 
 	while ((c = getchar()) != EOF)
-		++characters[c - '0'];
+		if (c >= 32 && c <= 126)       /* graphical characters only */
+			++characters[c - '0'];
 
-	for (i = 0; i <= MAXCHAR; ++i)          /* print value of each element (bar) */
+	for (i = 0; i <= MAXCHAR; i++)     /* print value of each element (bar) */
 		printf("%i ", characters[i]);
 	printf("\n");
 
 	printf("\nHorizontal Histogram:\n");
 
-	for (i = 0; i <= MAXCHAR; ++i) {
-		if (characters[i] != EMPTY)     /* skip label if no data */
-			printf("%c ", i + '0');     /* print histogram labels */
-		for (j = 1; j <= characters[i]; ++j)
+	for (i = 0; i <= MAXCHAR; i++) {
+		if (characters[i] != EMPTY)    /* skip label if no data */
+			printf("%c ", i + '0');    /* print histogram labels */
+		for (j = 1; j <= characters[i]; j++)
 			printf("* ");
-		if (characters[i] != EMPTY)     /* skip character */
+		if (characters[i] != EMPTY)    /* skip character */
 			printf("\n");
 	}
 
 	printf("\nVertical Histogram:\n");
 
 	longestBar = characters[0];
-	for (i = 0; i <= MAXCHAR; ++i)          /* find the longestBar */
+	for (i = 0; i <= MAXCHAR; i++)     /* find the longestBar */
 		if (longestBar < characters[i] )
 			longestBar = characters[i];
 
-	while (longestBar != 0) {           /* print vertical histogram  */
-		for (i = 0; i <= MAXCHAR; ++i)
+	while (longestBar != 0) {          /* print vertical histogram  */
+		for (i = 0; i <= MAXCHAR; i++)
 			if (characters[i] != EMPTY) {
 				if (characters[i] - longestBar < 0)
 					printf("  ");
@@ -53,7 +54,7 @@ int main(void)
 		--longestBar;
 		printf("\n");
 	}
-	for (i = 0; i <= MAXCHAR; ++i)          /* print histogram labels */
+	for (i = 0; i <= MAXCHAR; i++)     /* print histogram labels */
 		if (characters[i] != EMPTY)
 			printf ("%c ", i + '0');
 	printf("\n");
