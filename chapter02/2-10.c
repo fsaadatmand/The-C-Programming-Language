162b82ae0acfe5ff3797f220876ab26943260b6a
cdude <cdude996@gmail.com>
2019-03-10 19:44:29 -0400

    simplify function

--- chapter02/2-10.c
+++ chapter02/2-10.c
@@ -6,47 +6,20 @@
 
 #include <stdio.h>
 
-#define MAXLINE       1000         /* maximum input line length */
-
 /* functions */
-int  getLine(char [], int);
-void toLowercase(char []);
+int lower(char c);
 
-/* getLine function: read a line into s, return length */
-int getLine(char s[], int lim)
-{
-	int c, i;
-
-	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
-		s[i] = c;
-
-	if (c == '\n') {
-		s[i] = c;
-		++i;
-	}
-
-	s[i] = '\0';
-
-	return i;
-}
-
-void toLowercase(char line[])
+/* lower: convert c to lower case; ASCII only */
+int lower(char c)
 { 
-	int  i;
-
-	for (i = 0; line[i] != '\0'; ++i)
-		line[i] = (line[i] >= 'A' && line[i] <= 'Z') ? (line[i] + 'a' - 'A')
-			: line[i];
+	return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
 }
 
 int main(void)
 {
-	char len;
-	char line[MAXLINE];
+	char c;
 
-	while ((len = getLine(line, MAXLINE)) > 0) {
-		toLowercase(line);
-		printf("%s", line);
-	}
+	while ((c = getchar()) != EOF)
+			printf("%c", lower(c));
 	return 0;
 }
