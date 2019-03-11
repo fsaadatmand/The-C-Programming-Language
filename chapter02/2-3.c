06a6373773c3f69d278f5c4c68cb1900338373c7
cdude <cdude996@gmail.com>
2019-03-10 23:18:54 -0400

    clean up

--- chapter02/2-3.c
+++ chapter02/2-3.c
@@ -14,65 +14,70 @@
 #define MAXLINE 1000
 
 /* functions */
-int htoi(char [], int);
+long unsigned htoi(char []);
 
-int htoi(char s[], int len)
+long unsigned htoi(char s[])
 {
-	int i;
-	int intValue = 0;
+	int i, len;
+	long unsigned intValue = 0;
 	int base10;
-	
-	for (i = 0; s[i] != '\0'; ++i) {
-		if (isxdigit(s[i])) {
-			if (isdigit(s[i])) 
-				base10 = s[i] - '0';
-			else 
-				if (s[i] == 'a')
-					base10 = 10;
-				 else if (s[i] == 'b')
-					base10 = 11;
-				 else if (s[i] == 'c')
-					base10 = 12;
-				 else if (s[i] == 'd')
-					base10 = 13;
-				 else if (s[i] == 'e')
-					base10 = 14;
-				 else if (s[i] == 'f')
-					base10 = 15;
-				 else if (s[i] == 'A')
-					base10 = 10;
-				 else if (s[i] == 'B')
-					base10 = 11;
-				 else if (s[i] == 'C')
-					base10 = 12;
-				 else if (s[i] == 'D')
-					base10 = 13;
-				 else if (s[i] == 'E')
-					base10 = 14;
-				 else if (s[i] == 'F')
-					base10 = 15;
-		intValue += pow(16, len - 1) * base10;	
-		--len;
-		} else
+
+	len = strlen(s);
+
+	i = 0;
+	if (s[i] == '0' && (s[i + 1] == 'x' || s[i + 1] == 'X')) {
+		i += 2;
+		len -= 2;
+	}
+
+	for ( ; s[i] != '\0'; i++, len--) {
+
+		if (!isxdigit(s[i]))
 			return -1;
+
+		if (isdigit(s[i])) 
+			base10 = s[i] - '0';
+		else if (s[i] == 'a')
+			base10 = 10;
+		else if (s[i] == 'b')
+			base10 = 11;
+		else if (s[i] == 'c')
+			base10 = 12;
+		else if (s[i] == 'd')
+			base10 = 13;
+		else if (s[i] == 'e')
+			base10 = 14;
+		else if (s[i] == 'f')
+			base10 = 15;
+		else if (s[i] == 'A')
+			base10 = 10;
+		else if (s[i] == 'B')
+			base10 = 11;
+		else if (s[i] == 'C')
+			base10 = 12;
+		else if (s[i] == 'D')
+			base10 = 13;
+		else if (s[i] == 'E')
+			base10 = 14;
+		else if (s[i] == 'F')
+			base10 = 15;
+
+	intValue += pow(16, len - 1) * base10;	
 	}
 	return intValue;
 }
 
 int main(void)
 {
-	int len;
-	int intValue;
+	unsigned long intValue;
 	char xString[MAXLINE];
 
 	printf("Enter a hexadecimal string: ");
 	scanf("%s", xString);
 
-	len = strlen(xString);
-
-	if ((intValue = htoi(xString, len)) >= 0)
-		printf("%i\n", intValue);
-	else
+	if ((intValue = htoi(xString)) < 0)
 		printf("Invalid hexadecimal number\n");
+	else
+		printf("%lu\n", intValue);
 	return 0;
 }
