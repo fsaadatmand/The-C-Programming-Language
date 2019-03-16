4c5fc7558f6545a8c37d1b5a372a888ee3255322
cdude <cdude996@gmail.com>
2019-03-16 17:48:10 -0400

    fix typo

--- chapter05/5-12/detab.c
+++ chapter05/5-12/detab.c
@@ -1,5 +1,5 @@
 /* 
- * Exercise 5-12. Extend entab and detab to accpt the shorthand
+ * Exercise 5-12. Extend entab and detab to accept the shorthand
  * 	entab -m +n
  * 	to mean tab stops every n columns, starting at column m. Choose convenient
  * 	(for the user) default behaviour.
