b1a364f235dd4adb6bd20138b80c48f18b83a419
cdude <cdude996@gmail.com>
2019-03-28 15:39:51 -0400

    improve answer

--- chapter04/4-8.c
+++ chapter04/4-8.c
@@ -2,53 +2,48 @@
  * Exercise 4-8. Suppose that there will never be more than one character of
  * pushback. Modify getch and ungetch accordingly.
  *
- * NOTE: see alternate version 4-8a.c
- *
  * Faisal Saadatmand
  */
 
 #include <stdio.h>
-#include <stddef.h>          /* for NULL */
 
 /* functions */
 int    getch(void);
 void   ungetch(int);
 
 /* globals */
-char    buf;                 /* buffer from ungetch */
-char    *bufp = NULL;        /* pointer to buf */
+int    buf;                 /* buffer from ungetch */
 
 /* getch: get a (possibly pushed back) character */
 int getch(void)
 {
-	char temp;
+	int c;
 
-	if (bufp) {
-		temp = *bufp;
-		bufp = NULL;
-		return temp;
-	}
-	return getchar();
+	if (buf) {
+		c = buf;
+	} else
+		c = getchar();
+	buf = 0;
+	return c;
 }
 
 /* ungerch: push character back on input */
 void ungetch(int c)
 {
-	if (!bufp)
-		bufp = &buf;         /* initialize buffer pointer */
-	*bufp = c;
+	if (buf)
+		printf("ungetch: too many characters\n");
+	else
+		buf = c;
 }
 
 /* test getch */
 int main(void)
 {
 	ungetch('A');
-	ungetch('B');
+	ungetch('B');                      /* should give error */
+	printf ("%c\n", getch());
 	ungetch('C');
-	ungetch('D');
-
-	putchar(getch());
-	printf ("\n");
+	printf ("%c\n", getch());
 
 	return 0;
 }
