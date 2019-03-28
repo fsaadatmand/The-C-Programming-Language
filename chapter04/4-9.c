e5c08efa8fb96933bc81d33f0f371d323eab1913
cdude <cdude996@gmail.com>
2019-03-28 15:36:38 -0400

    simplify answer

--- chapter04/4-9.c
+++ chapter04/4-9.c
@@ -2,6 +2,11 @@
  * Exercise 4-9. Our getch and ungetch do not handle a pushed-back EOF
  * correctly. Decide what their properties ought to be if an EOF is pushed
  * back, then implement your design. 
+ *
+ * NOTE: the conversion of EOF (-1) from int to char and back to int may not
+ * preserve negative value of EOF on some machines. Thus, we should declare buf[]
+ * as int but[] to avoid conversion from from int to char to int of EOF.
+ *
  * Faisal Saadatmand
  */
 
@@ -16,24 +21,19 @@ void   ungetch(int);
 void   ungets(char []);
 
 /* globals */
-char   buf[BUFSIZE];         /* buffer from ungetch */
+int    buf[BUFSIZE];         /* buffer from ungetch */
 int    bufp;                 /* next free position in buf */
 int    pushedEOF;            /* signals EOF has been pushed-back */
 
 /* getch: get a (possibly pushed back) character */
 int getch(void)
 {
-	return (bufp > 0) ? buf[--bufp] : (pushedEOF) ? EOF : getchar();
+	return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
 /* ungerch: push character back on input */
 void ungetch(int c)
 {
-	if (c == EOF) {
-		pushedEOF = 1;
-		return;
-	}
-
 	if (bufp >= BUFSIZE)
 		printf("ungetch: too many characters\n");
 	else
@@ -45,11 +45,11 @@ int main(void)
 {
 	int c;
 
+	ungetch(EOF);
 	ungetch('A');
 	ungetch('B');
 	ungetch('C');
 	ungetch('D');
-	ungetch(EOF);
 
 	while ((c = getch()) != EOF)
 		putchar(c);
