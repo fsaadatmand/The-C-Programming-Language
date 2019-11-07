1ecacc6d9c3faf3eb6d83b4f2f1efc0323e780a3
cdude <cdude996@gmail.com>
2019-11-07 15:32:12 -0500

    minor change

--- chapter01/1-10.c
+++ chapter01/1-10.c
@@ -1,7 +1,8 @@
 /*
  * Exercise 1-10. Write a program to copy its input to its output, replacing
  * each tab by \t, each backspace by \b, and each backslash by \\. This makes
- * tabs and backspaces visible in an
+ * tabs and backspaces visible in an unambiguous way.
+ *
  * By Faisal Saadatmand
  */
 
