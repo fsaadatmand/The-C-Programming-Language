bc19ff1acc86aa8003adad2a24e8a0871330adf0
cdude <cdude996@gmail.com>
2019-11-07 15:31:25 -0500

    minor change

--- chapter01/1-9.c
+++ chapter01/1-9.c
@@ -1,6 +1,7 @@
 /*
  * Exercise 1-9. Write a program to copy its input to its output, replacing
  * each string of one or more blanks by a single blank.
+ *
  * By Faisal Saadatmand
 */
 
@@ -14,7 +15,7 @@ int main(void)
 	int c;          /* current input character */
 	int blank;      /* flag for the status of the previous character */
 
-	blank = NO;     /* perhaps unnecessary */
+	blank = NO;
 	while ((c = getchar()) != EOF) {
 		if (c != ' ')
 			blank = NO;
