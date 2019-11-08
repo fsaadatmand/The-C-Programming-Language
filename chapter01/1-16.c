a762694ff17197c5ddde704b5ec5dfead8e2c34c
cdude <cdude996@gmail.com>
2019-11-08 00:08:28 -0500

    revised answer

--- chapter01/1-16.c
+++ chapter01/1-16.c
@@ -1,14 +1,14 @@
 /*
  * Exercise 1-16. Revise the main routine of the longest-line program so it
- * will correctly print the length of arbitrary long input lines, and as much
- * as possible of the text.
- * Note: changed getline() name to getLine().
+ * will correctly print the length of arbitrary long input lines, and print as
+ * much as possible of the text.
+ *
  * By Faisal Saadatmand
  */
 
 #include <stdio.h>
 
-#define MAXLINE 1000          /* maximum input line length */
+#define MAXLEN 1000       /* maximum input line length */
 
 /* functionst */
 int  getLine(char [], int);
@@ -44,31 +44,31 @@ void copy(char to[], char from[])
 
 int main(void)
 {
-	int  len;                 /* current line length */
-	int  max;                 /* maximum length seen so far */
-	char line[MAXLINE];       /* current input line */
-	char longest[MAXLINE];    /* longest line saved here */
+	int len;                /* current line length */
+	int nextLen;            /* next line length */
+	int max;                /* maximum length seen so far */
+	char line[MAXLEN];      /* current input line */
+	char nextLine[MAXLEN];  /* next input line */
+	char longest[MAXLEN];   /* longest line saved here */
 
 	max = 0;
-	while ((len = getLine(line, MAXLINE)) > 0)
-		if (len == MAXLINE - 1 &&   /* test for length and previous input */
-				line[MAXLINE - 1] != '\n') {
-			copy(longest, line);            /* save the string */
-			longest[MAXLINE - 2] = '\n';    /* insert a newline character before
-											   the null character */
-			max = len;
-			max += getLine(line, MAXLINE); /* save the value of max */
-		} else if (len > max) {
+	while ((len = getLine(line, MAXLEN)) > 0) {
+		if (len == MAXLEN - 1) { /* is line longer than buffer size? */
+			line[MAXLEN - 1] = '\n';
+			nextLen = len;
+			while (nextLen == MAXLEN - 1) { /* find the line's length */
+				nextLen = getLine(nextLine, MAXLEN);
+				len += nextLen;
+			}
+		}
+		if  (len > max) {
 			max = len;
 			copy(longest, line);
 		}
-
-	if (max > 0)                            /* there was a line */
-		printf("%s", longest);
-
-	if (max >= MAXLINE - 1) {               /* print warning message */
-		printf("Warning: The longest line is %i characters long.\n", max);
-		printf("\t  Only showing the first %i characters\n", MAXLINE);
 	}
+
+	if (max > 0)             /* there was a line */
+		printf("%s -> %i\n", longest, max);
+
 	return 0;
 }
