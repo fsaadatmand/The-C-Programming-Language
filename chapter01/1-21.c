b95235fc35b8e818e9afee5b948ac9b9d441097c
cdude <cdude996@gmail.com>
2019-11-08 20:12:30 -0500

    simplified code

--- chapter01/1-21.c
+++ chapter01/1-21.c
@@ -3,24 +3,22 @@
  * minimum number of tabs and blanks to achieve the same spacing. When either a
  * tab or a single blank would suffice to reach a tab stop, which should be give
  * preference?
+ *
  * By Faisal Saadatmand
  */
 
 /*
- * Answer: in such a case, a signal space is given preference, because otherwise a change 
- * to the value of the tab stop (n) would distort the single space.
+ * Answer: a signal space is given preference, because otherwise a change to
+ * the value of the tab stop (n) would distort the single space.
  */
 
 #include <stdio.h>
 
 #define MAXLINE 1000
 #define N       4             /* default tabstop for every n columns */
-#define YES     1
-#define NO      0
 
 /* functions */
 int  getLine(char [], int);
-int  count(char [], char, int);
 void entab(char [], char []);
 
 /* getLine function: read a line into s, return length */
@@ -38,47 +36,34 @@ int getLine(char s[], int lim)
 	return i;
 }
 
-/* count function: counts the occurrences of a character in a string */ 
-int count(char s[], char c, int p)
-{
-	int nC = 0;               /* number of c occurrences in s[] */
-	while (s[p] == c) {
-		++p;
-		++nC;
-	}
-	return nC;
-}
-
 /* entab function: replaces blanks with the minimum number of tabs and blanks */
-void entab(char line[], char modLine[])
+void entab(char line[], char output[])
 {
 	int i;                    /* index for read line */
 	int j = 0;                /* index for modified (written) line */
-	int toTabStop;            /* number of blanks to the next tab stop */
-	int blanks;               /* size of the string of blanks */
-	int copyChar;             /* control flag: write to modline */
-
+	int nblanks;              /* number of blanks in a series of blanks */
+	int tabs, remaing_blanks;
+	
+	j = 0;
 	for (i = 0; line[i] != '\0'; ++i) {
-		copyChar = YES;
 		if (line[i] == ' ') {
-			toTabStop = N - (i % N);
-			blanks = count(line, ' ', i);
-			if (blanks > toTabStop) /* roll back counter to the last tab stop */
-				blanks = toTabStop;
-			if (blanks >= 2 && blanks >= toTabStop) { /* skip single blanks and
-														  not tab blanks */
-				modLine[j] = '\t';
-				++j;
-				i += toTabStop - 1;
-				copyChar = NO;
-			}
-		}
-		if (copyChar) {
-			modLine[j] = line[i];
-			++j;
-		}
+			nblanks = 1;
+			/* count the number trailing blanks, if any */
+			while (line[++i] == ' ')
+				++nblanks;
+			--i;
+			tabs = nblanks / N;
+			remaing_blanks = nblanks % N;
+			/* insert tabs */
+			while (tabs-- > 0)
+				output[j++] = '\t';
+			/* insert remaining blanks */
+			while (remaing_blanks-- > 0)
+				output[j++] = ' ';
+		} else 
+			output[j++] = line[i];
 	}
-	modLine[j] = '\0';
+	output[j] = '\0';
 }
 
 int main(void)
@@ -90,5 +75,6 @@ int main(void)
 		entab(line, modLine);
 		printf("%s", modLine);
 	}
+
 	return 0;
 }
