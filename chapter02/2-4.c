32c3a000a9b5c87654f11c18cc6d8244327179b3
cdude <cdude996@gmail.com>
2019-11-08 23:23:59 -0500

    remove flag variable match

--- chapter02/2-4.c
+++ chapter02/2-4.c
@@ -1,12 +1,13 @@
 /*
  * Exercise 2-4. Write an alternative version of squeeze(s1,s2) that deletes
  * each character in s1 that matches any character in the string s2.
+ *
  * By Faisal Saadatmand
  */
 
 #include <stdio.h>
 
-#define MAXCHAR 100
+#define MAXCHAR 1000
 
 /* functions */
 void squeeze(char [], char []);
@@ -14,27 +15,23 @@ void squeeze(char [], char []);
 void squeeze(char s1[], char s2[])
 {
 	int i, j, k;
-	int match = 0;
 
-	i = j = k = 0;
+	k = 0;
 	for (i = 0; s1[i] != '\0'; ++i) {
-		for (j = 0; match != 1 && s2[j] != '\0'; ++j)
-			if (s1[i] == s2[j])
-				match = 1;
-		if (!match)
+		for (j = 0; s2[j] != '\0' && s1[i] != s2[j]; ++j)
+			;
+		if (s2[j] == '\0') /* match not found */
 			s1[k++] = s1[i];
-		match = 0;
 	}
 	s1[k] = '\0';
 }
 
 int main(void)
 {
-	char string1[MAXCHAR] = { "*from *this- *str*-ing *** ---" };
+	char string1[MAXCHAR] = { "clean* *this- *str*-ing *** ---" };
 	char string2[MAXCHAR] = { "*-" };
 
 	squeeze(string1, string2);
-
 	printf("%s\n", string1);
 
 	return 0;
