79329fb4d6334409be9fd4614e33086bafc2fa13
cdude <cdude996@gmail.com>
2019-11-10 15:09:01 -0500

    fix answer

--- chapter02/2-6.c
+++ chapter02/2-6.c
@@ -2,30 +2,32 @@
  * Exercise 2-6. Write a function setbits(x,p,n,y) that returns x with the n
  * bits that begin at position p set to the rightmost n bits of y, leaving the
  * other bits unchanged.
+ *
  * by Faisal Saadatmand
- * */
+ */
 
 #include <stdio.h>
 
 /* functions */
-unsigned int setbits(unsigned int, int, int, unsigned);
+unsigned int setbits(unsigned, int, int, unsigned);
 
-unsigned int setbits(unsigned int x, int p, int n, unsigned y)
+unsigned int setbits(unsigned x, int p, int n, unsigned y)
 {
-	unsigned mask = ~(~0 << n);    /* mask to extract n bits from y */
-	unsigned xORy;
+	unsigned nbits;
 
-	xORy = x | (y & mask) << p;   /* move bits to p, OR with x to set them */
-	return xORy;
+	nbits = ~(~0 << n); /* mask to extract rightmost n bits */
+
+	return (x & ~(nbits << p)) | ((y & nbits) << p);
 }
 
 int main(void)
 {
-	unsigned int x = 0XF8FF;
-	unsigned int y = 0XF0A2;
+	unsigned x = 0XF8FF;
+	unsigned y = 0XF0A2;
 	int p = 8;                   /* starting position of bits */
-	int n = 2;                   /* number of bits to set */
+	int n = 8;                   /* number of bits to set */
 
 	printf("%x\n", setbits(x, p, n, y)); 
+
 	return 0;
 }
