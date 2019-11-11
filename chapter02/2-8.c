c4565c38ceef04052ba8e157cd7a328be6379919
cdude <cdude996@gmail.com>
2019-11-10 19:11:04 -0500

    refine answer

--- chapter02/2-8.c
+++ chapter02/2-8.c
@@ -1,32 +1,45 @@
 /*
  * Exercise 2-8. Write a function rightrot(x,n) that returns the value of the
  * integer x rotated to right by n positions.
+ *
  * By Faisal Saadatmand
  */
 
 #include <stdio.h>
 
 /* functions */
-unsigned int rightrot(unsigned int, int);
+unsigned rightrot(unsigned, int);
+size_t int_size();
 
-unsigned int rightrot(unsigned int x, int n)
+/* int_size: determine the size of an integer storged on this machine */
+size_t int_size()
 {
-	int size;
-	unsigned int bits = 0;
-	unsigned int rotatedX = 0;
+	int x = ~0;
+	size_t i = 0;
+
+	while (x != 0) {
+		x <<= 1;
+		++i;
+	}
+
+	return i;
+}
+
+unsigned rightrot(unsigned x, int n)
+{
+	unsigned size, bits;
 	
-	size = (sizeof(unsigned int)) * 8;    /* never assume size of int */
+	size = int_size();     /* never assume size of an int */
+	n = n % size;          /* scale down the shift count to a defined range */
+	bits = x & ~(~0 << n); /* extract the n bits to be rotated */
 
-	bits = x & ~(~0 << n);                /* extract n bits to be rotated */
-	rotatedX = (x >> n) | bits << (size - n);
-
-	return rotatedX;
+	return (x >> n) | bits << (size - n);
 }
 
 int main(void)
 {
-	unsigned int x = 0XAF14E5CB;
-	int          n = 16;
+	unsigned x = 0XAF14E5CB;
+	int n = 8;
 
 	printf("%x\n", rightrot(x, n));
 
