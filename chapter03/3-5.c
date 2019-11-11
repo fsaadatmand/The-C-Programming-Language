f5ac4f8a7e1b057d97c12e36cf4c41fe5e54e3e7
cdude <cdude996@gmail.com>
2019-11-11 16:23:35 -0500

    simplify code

--- chapter03/3-5.c
+++ chapter03/3-5.c
@@ -2,76 +2,70 @@
  * Exercise 3-5. Write the function itob(n,s,b) that converts the integer n
  * into a base b character representation in the string s. In particular,
  * itob(n,s,16) formats s as a hexadecimal integer in s.
+ *
  * By Faisal Saadatmand
  */
 
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
 
+#define MAXLEN 1000
+
 /* functions */
 void reverse(char []);
-void itob(int, char [], int);
+void itob(unsigned, char [], int);
 
-/* reverse function: reverse string s in place */
+/* reverse: reverse string s in place */
 void reverse(char s[])
 {
 	int c, i, j;
 
-	for (i = 0, j = strlen(s) - 1; i < j; i++, j --) {
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
 		c = s[i];
 		s[i] = s[j];
 		s[j] = c;
-		--j;
 	}
 }
 
-/* itob: convert number into a base base character representation in the string  s */
-void itob(int number, char convertedNumber[], int base)
+/* itob: converts the integer n into a base b character representation in the
+ * string s. */
+void itob(unsigned n, char s[], int b)
 {
-	int i, sign;
-	unsigned int uNumber, nextDigit;
-	const char baseDigits[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
-		                          '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+	int i, sign, digit;
+	const char baseDigits[16] = { '0', '1', '2', '3', '4', '5', '6', '7',
+								  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
-	sign = 0;
-	if (number < 0)                  /* record sign */
-		sign = 1;
-
-	uNumber = (unsigned) number;     /* work in two's complement number system */
-
-	if (sign > 0 && base == 10)      /* convert back to decimal for base 10 */
-		uNumber = ~uNumber + 1;
+	if (b == 10 && (sign = n) < 0)
+		n = -n; /* or you can use ~n + 1 */
 
 	i = 0;
-	do {                             /* generate digits in reverse order */
-		nextDigit = uNumber % base;  /* get next digit */
-		uNumber /= base;
-		convertedNumber[i] = baseDigits[nextDigit];
-		++i;
-	} while (uNumber != 0);
+	do {        /* generate digits in reverse order */
+		digit = n % b; /* get next digit */
+		s[i++] = baseDigits[digit];
+	} while ((n /= b) > 0);
 
-	if (sign > 0 && base == 10)      /* add sign symbol for base 10 */
-		convertedNumber[i++] = '-';
+	if (b == 10 && sign < 0) /* add sign symbol for base 10 */
+		s[i++] = '-';
 
-	convertedNumber[i] = '\0';
+	s[i] = '\0';
 
-	reverse(convertedNumber);
+	reverse(s);
 }
 
 int main(void)
 {
-	int number, base;
-	char stringNumber[64];
+	int i;
+	char str[MAXLEN];
 
-	printf("Enter an integer to convert: ");
-	scanf("%i", &number); 
-
-	printf("Enter base: ");
-	scanf("%i", &base);
-
-	itob(number, stringNumber, base);
-	
-	printf("%s\n", stringNumber);
+	printf("converting %i and %i to\n", INT_MIN, INT_MAX);
+	for (i = 2; i <= 16; ++i) {
+		printf("Base% i:\n", i);
+		itob(INT_MIN, str, i);
+		printf(" %s\n",  str);
+		itob(INT_MAX, str, i);
+		printf(" %s\n", str);
+	}
 
 	return 0;
 }
