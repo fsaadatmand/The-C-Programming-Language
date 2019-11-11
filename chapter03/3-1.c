371b3bdace5c98c13007742da07167ac879e9d0a
cdude <cdude996@gmail.com>
2019-11-11 16:24:30 -0500

    minor tweaks

--- chapter03/3-1.c
+++ chapter03/3-1.c
@@ -3,10 +3,13 @@
  * would suffice (at the price of more tests outside.) Write a version with
  * only one test inside the loop and measure the difference in run-time.
  *
- * Answer: binSearch_alt algorithm is faster because it has one less
- * comparison in the loop; However it doesn't stop when the number is found in
- * the loop.
  * By Faisal Saadatmand
+ */
+
+/*
+ * Answer: this version  is faster because it has one less comparison in the
+ * loop; However, it doesn't stop the loop as soon as the match is found.
+ * the loop.
  */
 
 #include <stdio.h>
@@ -15,7 +18,6 @@
 
 /* functions */
 int binSearch(int, int [], int);
-int binSearch_alt(int, int [], int);
 
 /* binSearch: find x in value[0] <= value[1] <= ... <= value[n - 1] */
 int binSearch(int x, int v[], int n)
@@ -24,26 +26,6 @@ int binSearch(int x, int v[], int n)
 
 	low = 0;
 	high = n - 1;
-	
-	while (low <= high) {
-		mid = (low + high) / 2;
-		if (x < v[mid])
-			high = mid - 1;
-		else if (x > v[mid])
-			low = mid + 1;
-		else
-			return mid;    /* match found */
-	}
-	return -1;             /* no match */
-}
-
-int binSearch_alt(int x, int v[], int n)
-{
-	int low, high, mid;
-
-	low = 0;
-	high = n - 1;
-
 	while (low < high) {
 		mid = (low + high) / 2;
 		if (v[mid] < x)
@@ -52,19 +34,21 @@ int binSearch_alt(int x, int v[], int n)
 			high = mid;
 	}
 	if (v[low] == x)
-		return low;
-	return -1;
+		return low; /* found match */
+	return -1; /* no match */
 }
 
 int main(void)
 {
 	int array[SIZE] = { 2, 4, 5, 6, 8, 9, 12, 16, 20, 32, 40, 78 };
-	int number = 0;
+	int input, number;
 
-	printf("Enter number to search in the array: ");
-	while (scanf("%i", &number) != EOF) {
-		printf("%i\n", binSearch_alt(number, array, SIZE));
+	while (1) {
 		printf("Enter number to search in the array: ");
+		input = scanf("%i", &number);
+		if (!input || input == EOF)
+			return -1;
+		printf("%i\n", binSearch(number, array, SIZE));
 	}
 	return 0;
 }
