4becd4a7ec461b86cbb6c15d1587e5076f4280e2
cdude <cdude996@gmail.com>
2019-11-11 16:24:05 -0500

    fix whitespace

--- chapter03/3-6.c
+++ chapter03/3-6.c
@@ -2,6 +2,7 @@
  * Exercise 3-6. Write a version of itoa that accepts three arguments instead
  * of two. The third argument is a minimum field width; the converted number
  * must be padded with blanks on the left if necessary to make it wide enough.
+ *
  * By Faisal Saadatmand
 */
 
