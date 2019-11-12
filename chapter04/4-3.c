b5ca7441bc30414fc683b4da882ab753d21c0d6a
cdude <cdude996@gmail.com>
2019-11-11 20:13:13 -0500

    fix whitespace

--- chapter04/4-3.c
+++ chapter04/4-3.c
@@ -1,6 +1,7 @@
 /* 
  * Exercise 4-3. Given the basic framework, it's straightforward to extend the
  * calculator. Add the modulus (%) operator and provisions for negative numbers
+ *
  * By Faisal Saadatmand
  */
 
