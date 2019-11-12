6baec96133dff855d747797f697807f8d2bb3e13
cdude <cdude996@gmail.com>
2019-11-12 00:33:38 -0500

    refined answer

--- chapter04/4-12.c
+++ chapter04/4-12.c
@@ -2,57 +2,56 @@
  * Exercise 4-12: Adapt the idea of printd to write a recursive version of
  * itoa; that is, convert an integer into a string by calling a recursive
  * routine.
+ *
  * By Faisal Saadatmand
+ */
+
+/*
+ * NOTE: We could have made i a static variable; however, that is problematic
+ * because its value will persists throughout the entire life of the program, and
+ * thus, multiple calls to itoa will use the value of i from the previous call,
+ * which will result in an undesirable outcome.
  */
 
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
 
+#define MAXLEN 1000
+
 /* functions */
-void itoa(int, char []);
+int itoa(unsigned, char [], int);
 
-/* itoa: convert n to characters in s. Recursive version */
-void itoa(int n, char s[])
+int itoa(unsigned n, char s[], int i)
 {
-	int lastDigit;
-	int static i;                         /* note static type */
+	int sign;
 
-	lastDigit = 0;
-	if (n == INT_MIN) {                   /* add check for minimum signed int */
-		lastDigit = (n % 10 * -1) + '0';  /* extract and save the last digit */
-		n += 1;                           /* reduce n to fit into INT_MAX */
-	}
-
-	if (n < 0) {
-		s[i++] = '-';                     /* store leading negative sign */
+	if ((sign = n) < 0) {
+		s[i++] = '-';
 		n = -n;
 	}
-
 	if (n / 10)
-		itoa(n / 10, s);                  /* recursive call */
-
+		i = itoa(n / 10, s, i); /* recursive call */
 	s[i++] = n % 10 + '0';
-
-	if (lastDigit > 0) {
-		--i;
-		s[i++] = lastDigit;               /* put back saved last digit */
-	}
-
 	s[i] = '\0';
+	return i; /* return the updated value of i */
 }
 
 int main(void)
 {
-	int intValue;
-	char stringNumber[64];
+	char str[MAXLEN];
 
-	printf("Enter integer to convert to a string: ");
-	scanf("%i", &intValue);
+	itoa(996, str, 0);
+	printf("%s\n", str);
 
-	itoa(intValue, stringNumber);
+	itoa(-2345, str, 0);
+	printf("%s\n", str);
 
-	printf("%s\n", stringNumber);
+	itoa(INT_MAX, str, 0);
+	printf("%s\n", str);
+
+	itoa(INT_MIN, str, 0);
+	printf("%s\n", str);
 
 	return 0;
 } 
