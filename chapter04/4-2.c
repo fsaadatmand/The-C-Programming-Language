74bcd5738fc142e2c436e63e4c5a5b8a141ea047
cdude <cdude996@gmail.com>
2019-11-11 20:00:54 -0500

    simplify code

--- chapter04/4-2.c
+++ chapter04/4-2.c
@@ -1,14 +1,16 @@
 /*
  * Exercise 4-2. Extend atof to handle scientific notation of the form
- * 	123.45e-6
+ *
+ * 		123.45e-6
+ *
  * where a floating-point number may be followed by e or E and an optionally
- * signed exponent. 
+ * signed exponent.
+ *
  * By Faisal Saadatmand
  */
 
 #include <stdio.h>
 #include <ctype.h>
-#include <math.h>
 
 /* functions */
 double atof(char []);
@@ -16,55 +18,54 @@ double atof(char []);
 /* atof: convert string s to double */
 double atof(char s[])
 {
-	int    i, sign, expSign;
+	int i, sign, expSign;
 	double val, power, exponent;
 
-	for (i = 0; isspace(s[i]); ++i)               /* skip whitespace */
+	for (i = 0; isspace(s[i]); ++i)           /* skip whitespace */
 		;
 
 	sign = (s[i] == '-') ? -1 : 1;
 	if (s[i] == '+' || s[i] == '-')
 		++i;
 	
-	for (val = 0.0; isdigit(s[i]); i++)
+	for (val = 0.0; isdigit(s[i]); ++i)
 		val = 10.0 * val + (s[i] - '0');
 	
 	if (s[i] == '.')
-		i++;
+		++i;
 
-	for (power = 1.0; isdigit(s[i]); i++) {
+	for (power = 1.0; isdigit(s[i]); ++i) {
 		val = 10.0 * val + (s[i] - '0');
 		power *= 10.0;
 	}
 
-	if (s[i] != '\0') {                           /* handle scientific notation */
-		if (s[i] == 'e' || s[i] == 'E')
-			++i;
-		else
-			return -1;
+	if (tolower(s[i]) == 'e') /* handle scientific notation */
+		++i;
 
-		expSign = (s[i] == '-') ? -1 : 1;         /* record the exponent sign */
-		if (s[i] == '+' || s[i] == '-')
-			++i;
+	expSign = (s[i] == '-') ? -1 : 1; /* record  exponent's sign */
+	if (s[i] == '+' || s[i] == '-')
+		++i;
 
-		for (exponent = 0.0; isdigit(s[i]); i++)  /* extract the exponent */
-			exponent = 10.0 * exponent + (s[i] - '0');
+	for (exponent = 0.0; isdigit(s[i]); i++)  /* extract the exponent */
+		exponent = 10.0 * exponent + (s[i] - '0');
 
-		return (sign * val / power) * pow(10, expSign * exponent);
+	while (exponent-- != 0)         /* adjust power according to exponent */
+		power = (expSign > 0) ? power / 10: power * 10; 
 
-	} else
-		return sign * val / power;
+	return sign * val / power;
 }
 
 int main(void)
 {
-	char number[64]  = { "123.45" };
-	char number2[64] = { "123.45e-6" };
-	char number3[64] = { "123.45e+6" };
+	char number[]  = "123.45";
+	char number2[] = "123.45e-6";
+	char number3[] = "123.45e+6";
+	char number4[] = "-123.45E+6";
 
 	printf("%f\n", atof(number));
 	printf("%f\n", atof(number2));
 	printf("%f\n", atof(number3));
+	printf("%f\n", atof(number4));
 
 	return 0;
 }
