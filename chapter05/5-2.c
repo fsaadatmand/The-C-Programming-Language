26b614f5f7aa014cbfc2479a8fa676c813d89340
cdude <cdude996@gmail.com>
2019-11-13 13:20:07 -0500

    revise answer

--- chapter05/5-2.c
+++ chapter05/5-2.c
@@ -1,15 +1,20 @@
 /*
  * Exercise 5-2. Write getfloat, the floating-point anolog of getint. What type
- * does getfloat return as its function value? int
+ * does getfloat return as its function value?
  *
  * By Faisal Saadatmand
+ */
+
+/*
+ * Answer: It should return an int, which then could be used to check if the
+ * operation was successful or not or if the EOF was reached. See main
+ * function.
  */
 
 #include <stdio.h>
 #include <ctype.h>
 
 #define BUFSIZE  100
-#define SIZE     100
 
 /* functions */
 int  getch(void);
@@ -17,15 +22,15 @@ void unget(int);
 int  getfloat(double *);
 
 /* global */
-int    buf[BUFSIZE];         /* buffer from ungetch */
-int    bufp = 0;             /* next free position in buf */
+int buf[BUFSIZE];         /* buffer from ungetch (can handle EOF push back) */
+int bufp = 0;             /* next free position in buf */
 
-int getch(void)              /* get a (possibly pushed back) character */
+int getch(void)           /* get a (possibly pushed back) character */
 {
 	return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
-void ungetch(int c)          /* push character back on input */
+void ungetch(int c)       /* push character back on input */
 {
 	if (bufp >= BUFSIZE)
 		printf("ungetch: too many characters\n");
@@ -36,45 +41,36 @@ void ungetch(int c)          /* push character back on input */
 /* getfloat: get next float from input into *pn */
 int getfloat(double *pn)
 {
-	int c, sign, signChar, decimalPoint = 1;
+	int c, sign;
+	double i;
 
-	while (isspace(c = getch()))    /* skip white space */
+	while (isspace((c = getch()))) /* skip whitespace */
 		;
-	if (!isdigit(c) && c != EOF && c != '+' && c != '-' && c != '.')
-		return 0;            /* it is not a number */
-	sign = (c == '-') ? -1 : 1;
-	if (c == '+' || c == '-') {
-		signChar = c;
-		if (!isdigit(c = getch())) {
-			if (c != EOF)
-				ungetch(c);
-			ungetch(signChar);
-			return signChar;
-		}
+	if (!isdigit(c) && c != '.' && c != '+' && c != '-') {
+		ungetch(c); /* it is not a number */
+		return 0;
 	}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-')
+		if (!isdigit((c = getch()))) /* is the sign is follwed by a digit? */
+			return 0; /* it is not a number */
 	for (*pn = 0; isdigit(c); c = getch())
 		*pn = 10 * *pn + (c - '0');
 	if (c == '.')
-		while (isdigit(c = getch())) {
-			decimalPoint *= 10; 
-			*pn += (double) (c - '0') / decimalPoint;
-		}
+		for (i = 10.0F; isdigit(c = getch()); i = i * 10.0F)
+			*pn += (c - '0') / i;
 	*pn *= sign;
-	if (c != EOF)
-		ungetch(c);
+	ungetch(c);
 	return c;
 }
 
 int main(void)
 {
-	int n, input;
-	double array[SIZE];
+	int input;
+	double num;
 
-	for (n = 0; n < SIZE && (input = getfloat(&array[n])) != EOF; ++n) {
-		if (input == 0 || input == '-' || input == '+')
-			break;
-		printf(" %g", array[n]);
-	}
-	printf("\n");
+	while ((input = getfloat(&num)) && input != EOF)
+		printf("%g\n", num);
+
 	return 0;
 }
