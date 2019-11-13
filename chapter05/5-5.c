78606839a3b1baddbd59de3a50d96fd463059733
cdude <cdude996@gmail.com>
2019-11-13 16:19:05 -0500

    revise answer

--- chapter05/5-5.c
+++ chapter05/5-5.c
@@ -3,6 +3,7 @@
  * strncmp, which operate on at most the first n characters of their argument
  * strings. For example, strncpy(s, t, n) copies at most n characters of t to
  * s. Full descriptions are in Appendix B.
+ *
  * By Faisal Saadatmand
  */
 
@@ -12,81 +13,58 @@
 #define MAXCHAR 32
 
 /* functions */
-char *strnCpy(char *, char *, int);
-char *strnCat(char *, char *, int);
+char *strnCpy(char *, char *, const size_t);
+char *strnCat(char *, char *, const size_t);
+int strnCmp(const char *, const char *, const size_t);
 
 /* strnCpy: copy at most n characters of string t to s. Returns s. Pad with
  * '\0's if t has fewer than n characters */
-char *strnCpy(char *s, char *t, int n)
+char *strnCpy(char *s, char *t, const size_t n)
 {
-	int nChar;                     /* number of characters copied */
-	int t_len;                     /* length of source string */
+	size_t i;
 
-	t_len = strlen(t);
-	nChar = 0;
+	for (i = 0; i < n; ++i)
+		if (!(*s++ = *t++)) /* copy the character */
+			*s = '\0'; /* pad with '\0's, if t's length is < n */
+	*s = '\0'; /* add the terminating character */
 
-	if (n <= 0)                    /* invalid inputs */
-		return t;
-
-	while (nChar < n) {            /* copy exactly n characters */
-		*s++ = *t++;
-		++nChar;
-	}
-	*s = '\0';                     /* terminate with null character */
-
-	if (t_len < n) 
-		while (nChar != n) {       /* pad t with '\0' */
-			*s++ = '\0';
-			++nChar;
-		}
 	return s - n;
-
 }
 
 /* strnCat: concatenate at most n characters of string t to string s, terminate
- * s with '\0'; return s*/
-char *strnCat(char *s, char *t, int n)
+ * s with '\0'; return s */
+char *strnCat(char *s, char *t, const size_t n)
 {
-	int s_len;                      /* length of s */
-	int nChar = 0;
+	size_t i, s_len, t_len, lim;
 
-	s_len = strlen(s);
-	s += s_len;                     /* find the end of s */
-
-	while (nChar < n) {
-		*s++ = *t++;
-		++nChar;
-	}
-
+	s += (s_len = strlen(s)); /* advance pointer */
+	lim = (n > (t_len = strlen(t))) ? t_len : n; /* scale down n */
+	for (i = 0; i < lim && (*s++ = *t++); ++i)
+		;
 	*s = '\0';
-
-	return s - s_len - nChar;
+	return s - s_len - i;
 }
 
 /* strnCmp: compare at most n characters of string s to string t; return < 0
  * if s < t, 0 if s == t, or > 0 if s > t. */
-int strnCmp(char *s, char *t, int n)
+int strnCmp(const char *s, const char *t, const size_t n)
 {
-	while (*s == *t) {
-		++s;
-		++t;
-		--n;
-		if (n == 0)
+	size_t i; 
+
+	for (i = 1; i < n && *s == *t; ++s, ++t, ++i)
+		if (*s == '\0')
 			return 0;
-	}
 	return *s - *t;
 }
 
 int main(void)
 {
-	char dest1[MAXCHAR];
-	char dest2[MAXCHAR] = { "It's a " };
-	char source[MAXCHAR] = { "copy me" };
-	char string1[MAXCHAR] = { "compare me" };
-	char string2[MAXCHAR] = { "compare me with other strings" };
+	char dest[MAXCHAR];
 
-	printf("%s\n", strnCpy(dest1, source, 4));
-	printf("%s\n", strnCat (dest2, source, 4));
-	printf("%i\n", strnCmp (string1, string2, 4));
+	printf("%s\n", strnCpy(dest, "copy me", 4));
+	printf("%s\n", strnCat(dest, "concatenate", 4));
+	printf("%i\n", strnCmp ("samee", "same", 4));
+	printf("%i\n", strnCmp ("samee", "same", 5));
+
 	return 0;
 }
