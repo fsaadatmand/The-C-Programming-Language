f78b92e4555fe8959814a9baacdaf22d82f082a2
cdude <cdude996@gmail.com>
2019-11-19 09:13:12 -0500

    simplify error check

--- chapter05/5-8.c
+++ chapter05/5-8.c
@@ -1,6 +1,7 @@
 /*
  * Exercise 5-8. There is no error checking in day_of_year or month_day. Remedy
  * this defect.
+ *
  * By Faisal Saadatmand
  */
 
@@ -20,14 +21,11 @@ int day_of_year(int year, int month, int day)
 {
 	int i, leap;
 
-	if ((month < 1 && month > 12) || (day < 1 && day > 31) || year < 0)
+	if (year < 0 || month < 1 ||  month > 12 || day < 1 || day > 31)
 		return -1;
-
 	leap = (year % 4 == 0 && year % 100) || year % 400 == 0;
-	
 	for (i = 1; i < month; i++)
 		day += daytab[leap][i];
-
 	return day;
 }
 
@@ -36,30 +34,29 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
 {
 	int i, leap;
 
+	if (year < 0 || yearday < 1 || yearday > 366) {
+		*pmonth = *pday =  0;
+		return;
+	}
 	leap = (year % 4 == 0 && year % 100) || year % 400 == 0;
-
 	for (i = 1; yearday > daytab[leap][i]; i++)
 		yearday -= daytab[leap][i];
-
-	(i < 1 || i > 12 || year < 0) ? (*pmonth = daytab[0][0]) : (*pmonth = i);
-	(yearday < 1 || yearday > 366 || year < 0) ? (*pday = daytab[0][0]) :
-		(*pday = yearday);
+	*pmonth = i;
+	*pday = yearday;
 }
 
 int main(void)
 {
 	int yearday, month, day;
 
-	if ((yearday = day_of_year(1977, 07, 22)) >= 0)
-		printf("%i day of the year\n", yearday);
+	if ((yearday = day_of_year(1986, 3, 35)) < 0)
+		printf("Invalid input\n");
 	else
-		printf("invalid input\n");
-
-	month_day(2020, 366, &month, &day);
-
-	if (month != 0 && day != 0)
+		printf("day of the year: %i\n", yearday);
+	month_day(2020, 367, &month, &day);
+	if (!month || !day)
+		printf("Invalid input\n");
+	else
 		printf("month %i, day %i\n", month, day);
-	else
-		printf("invalid input\n");
 	return 0;
 }
