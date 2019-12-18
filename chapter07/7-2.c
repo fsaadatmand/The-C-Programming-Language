d0e4da438503036bfa939a93d5d71328de3d7a10
cdude <cdude996@gmail.com>
2019-12-18 13:28:54 -0500

    revised code

--- chapter07/7-2.c
+++ chapter07/7-2.c
@@ -5,26 +5,40 @@
  *
  * By Faisal Saadatmand
  */
-#include <stdio.h>
-#include <ctype.h>
 
-#define MAXLENGTH 81
+#include <ctype.h>
+#include <stdio.h>
+
+#define MAXLEN  80 
+#define NCHAR   6  /* number of actually printed characters */
+
+/* inclen: increment line's length by the number of actually printed
+ * characters. Break the line if its too long. */
+int inclen(int len, int n)
+{
+	if ((len += n) < MAXLEN)
+		return len;
+	printf("\n");
+	return n;
+}
 
 int main(void)
 {
-	char c;
-	int len;                           /* length of currently read line */
+	int c;
+	int len; /* length of currently read line */
 
-	for (len = 0; (c = getchar()) != EOF; len++) {
-		if (len == MAXLENGTH) {        /* simple line breaking */
-			printf("\n");
-			len = 0;
-		}
-		if (!isgraph(c))              /* print in hexadecimal */
-			printf("%x", c);
-		else
+	len = 0;
+	while ((c = getchar()) != EOF)
+		if (!isgraph(c)) {
+			printf(" 0x%02o ", c); /* replace by NCHAR hex characters */
+			len = inclen(len, NCHAR); 
+			if (c == '\n') {
+				printf("\n");   /* break the line */
+				len = 0;
+			}
+		} else { /* graphical character */
 			printf("%c", c);
-	}
-	printf("\n");
+			len = inclen(len, 1);
+		}
 	return 0;
 }
