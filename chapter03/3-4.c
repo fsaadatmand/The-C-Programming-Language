98dd84cd8eacdced637754f0df101d9643f220bb
cdude <cdude996@gmail.com>
2019-12-27 14:16:11 -0500

    tweak answer
    Some minor tweaks and added a reference to an alternative approach.

--- chapter03/3-4.c
+++ chapter03/3-4.c
@@ -4,19 +4,24 @@
  * equal to -(2^wordsize-1). Explain why not. Modify it to print that value
  * correctly, regardless of the machine on which it runs. 
  *
- * ANSWER:
- * In two's complement number representation, the limit for a signed int ranges
- * from -(2^wordsize-1) to (2^wordsize-1)-1. The most significant bit is the
- * sign bit; however, it also holds a value for negative numbers; thus, making
- * the negative limit larger than the positive limit by a value of 1. When we
- * negate the largest negative number, -(2^wordsize-1), we get a number that is
- * equal to 2^wordsize-1, which is larger than the largest positive number,
- * (2^wordsize-1)-1. This will overflow the signed int and cause unexpected
- * results. To overcome this, we can use an unsigned int for n. The check for
- * whether n is negative or not is taking care of with the assigned of n to the
- * int variable sign, in which n is convert to a signed int. 
- *
  *  By Faisal Saadatmand
+ */
+
+/*
+ * ANSWER:
+ * In two's complement number representation, the range of values an int can
+ * hold is from -(2^wordsize-1) to (2^wordsize-1)-1. The most significant bit
+ * is the sign bit; however, it also holds a value for negative numbers; thus,
+ * making the negative limit larger than the positive limit by a value of 1.
+ * When we negate the largest negative number, -(2^wordsize-1), we get a number
+ * that is equal to 2^wordsize-1, which is larger than the largest positive
+ * number, (2^wordsize-1)-1. This will overflow a signed int and cause
+ * unexpected results. To overcome this, we can use an unsigned int for n. The
+ * check for whether n is negative or not is taking care of with the assigned
+ * of n to the int variable sign, in which n is convert to a signed int.
+ *
+ * Alternatively, we could avoid negating n altogether and use the abs function
+ * from stdlib.h to extract the digits. See 3-4a.c.
  */
 
 #include <stdio.h>
@@ -39,18 +44,19 @@ void itoa(unsigned n, char s[])
 	i = 0;
 	do {           /* generate digits in revered order */
 		s[i++] = n % 10 + '0'; /* get next digit */
-	} while ((n /= 10) > 0);   /* delete it */
+	} while ((n /= 10) != 0);   /* delete it */
 	if (sign < 0)
 		s[i++] = '-';
 	s[i] = '\0';
 	reverse(s);
 }
 
+/* reverse: reverse the order of the characters in the string s */
 void reverse(char s[])
 {
 	int c, i, j;
 
-	for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+	for (i = 0, j = strlen(s) - 1; i < j; ++i, --j) {
 		c = s[i];
 		s[i] = s[j];
 		s[j] = c;
@@ -61,6 +67,8 @@ int main(void)
 {
 	char str[MAXLEN];
 
+	itoa(0, str); /* do-while loop is necessary for 0 */
+	printf("%i -> %s\n", 0, str);
 	itoa(INT_MAX, str);
 	printf("%i -> %s\n", INT_MAX, str);
 	itoa(INT_MIN, str);
